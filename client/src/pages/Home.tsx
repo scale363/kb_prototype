3284d6a852e6bd0074fc7e333847cc5c9790418a
Alexander <filippov.wrk@gmail.com>
2026-01-05 08:05:00 +0000

    первая фраза

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { TextInputArea } from "@/components/TextInputArea";
 import { KeyboardContainer } from "@/components/KeyboardContainer";
 
 export default function Home() {
-  const [text, setText] = useState("Напиши босу что я не приду нафиг. ");
+  const [text, setText] = useState("привет, петя. Напиши босу что я не приду нафиг, проспал");
   const [cursorPosition, setCursorPosition] = useState(0);
   const [selectedText, setSelectedText] = useState("");
   const [previewText, setPreviewText] = useState("");
